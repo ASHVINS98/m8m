583256ae0a4d1c3c81a86dfc13fafef95f5cc176
ASHVINS98 <ashwani.singh@anrxsolutions.com>
2026-02-15 16:07:57 +0530

    feat:db prisma setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import { cn } from "@/lib/utils";
+import prisma from "@/lib/db";
 
-export default function Home() {
+const Page = async () => {
+  const users = await prisma.user.findMany();
+  console.log(users);
   return (
     <div>
       <h1 className="text-red-900">Home</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+export default Page;
