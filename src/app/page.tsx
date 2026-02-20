515f61f80fc6256b6459137c702f16e1cdd58db2
ASHVINS98 <ashwani.singh@anrxsolutions.com>
2026-02-20 12:37:02 +0530

    feat:inngest background workflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
-import { requireAuth } from "@/lib/auth-utils";
-import { caller } from "@/trpc/server";
-const Page = async () => {
-  await requireAuth();
-  const data = await caller.getUsers();
+"use client"
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+const Page = () => {
+  
+  const trpc = useTRPC();
+  const { data} = useQuery(trpc.getWorkflows.queryOptions())
+  const queryClient = useQueryClient()
+  const create = useMutation(trpc.createWorkflow.mutationOptions({
+    onSuccess(){
+      toast.success("Workflow creation triggered")
+    }
+  }))
   
   return (
     <div className='min-h-screen flex items-center justify-center'>
       Protected server component
       {JSON.stringify(data)}
+      <Button onClick={() => create.mutate()}>Create Workflow</Button>
     </div>
   );
 }
